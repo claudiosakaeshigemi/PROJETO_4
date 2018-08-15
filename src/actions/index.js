00d95f59b3b29a972fc2dbd5fb373c99a966f30c
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-15 14:14:52 -0300

    aula 127
    Rendering a List of Posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
-
 export const FETCH_POSTS = 'fetch_posts';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY ='?key=PROJETO_4';
+
+const API_KEY ='?key=MRR_PROJETO_4';
+
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
+    console.log(request);
     return{ 
         type: FETCH_POSTS,
         payload: request
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import PostReducer from './reducer_posts';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  
+  posts: PostsReducer
 });
 
 export default rootReducer;
