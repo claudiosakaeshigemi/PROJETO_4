051fda0e105099f18d4e3b26982afc4414aa2fe4
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-15 09:34:30 -0300

    aula 124
    Back to Redux - Index Action

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
-
+import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/post_index';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
@@ -16,5 +16,5 @@ ReactDOM.render(
       </div>
     </BrowserRouter>
 
-  </Provider>
+  </Provider> 
   , document.querySelector('.container'));
