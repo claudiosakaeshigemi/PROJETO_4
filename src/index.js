e4809996a9d52d207056aa11519c682650b8cc2c
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-16 20:23:01 -0300

    Aula 129
    A React Router Gotcha

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise   from 'redux-promise';
 import reducers  from './reducers';
 import PostIndex from './components/posts_index';
@@ -15,8 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div> 
-        <Route path="/" component= {PostIndex} />
+        <Switch>
         <Route path="/posts/new" component= {PostsNew} />
+        <Route path="/" component= {PostIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
 
