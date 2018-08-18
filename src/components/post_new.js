262ddd229c7cca8d7f899aa193276a5ca4a23db3
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-18 14:52:23 -0300

    Aula 141 Create Post Action Creator
    Aula 141 Create Post Action Creator

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,6 +1,8 @@
 import React , { Component} from 'react';
 import { Field, reduxForm } from 'redux-form';
 import {Link} from  'react-router-dom';
+import {connect }from 'react-redux';
+import { createPost} from '../actions/index';
 
 
 class PostsNew extends Component {
@@ -29,6 +31,7 @@ class PostsNew extends Component {
 
     onSubmit(values ) {
         console.log(values);
+        this.props.createPost(values);
     }
 
 
@@ -78,4 +81,6 @@ function validate(values ) {
 export default reduxForm({
     validate , 
     form: 'PostsNewForm'
-})  (PostsNew);
+})  (
+        connect(null, {createPost } ) (PostsNew)
+);
