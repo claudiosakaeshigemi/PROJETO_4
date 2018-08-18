48ed0f2f1b71380672c00ea4932f0f0cfa2168ff
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-18 17:45:48 -0300

    Aula 147 - Caching Records
    Aula 147 - Caching Records

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,27 +1,34 @@
 import React, {Component  } from 'react';
 import {connect } from 'react-redux';
-import { fetchPost} from '..//actions/index';
+import { fetchPost} from '../actions/index';
 
 
 
 class PostsShow extends Component {
     componentDidMount() {
-        const  {id } = this.props.match.params;
-        this.props.fetchPost();
+        const  { id } = this.props.match.params;
+        this.props.fetchPost(id);
     }
 
 
 
     render () {
+        const { post } = this.props;
+
+        if(!post){
+            return <div> Loading... </div>
+        }
         return( 
             <div>
-                Posts Show!
+                <h3> { post.title }</h3>
+                <h6> Categories: {post.categories}</h6>
+                <p>{post.content} </p>
             </div>
         );
     }
 }
 
-function mapStateToProps ({post} , ownProps ) {
+function mapStateToProps ({posts} , ownProps ) {
     return { post: posts[ownProps.match.params.id]};
 
 }
